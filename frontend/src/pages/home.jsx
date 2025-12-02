b3de689f6aa1e5f7c3add079e591bc3ed1169de5
Sameer  Dharmadhikari <sameerdharmadhikari21@gmail.com>
2025-12-02 21:07:53 +0530

    final name change

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,7 +26,7 @@ function HomeComponent() {
 
                 <div style={{ display: "flex", alignItems: "center" }}>
 
-                    <h2>Apna Video Call</h2>
+                    <h2>CallConnect</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
@@ -54,7 +54,7 @@ function HomeComponent() {
             <div className="meetContainer">
                 <div className="leftPanel">
                     <div>
-                        <h2>Providing Quality Video Call Just Like Quality Education</h2>
+                        <h2>Providing Quality Video Call</h2>
 
                         <div style={{ display: 'flex', gap: "10px" }}>
 
